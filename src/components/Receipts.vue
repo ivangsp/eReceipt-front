93abf5d7f7f3a611e9f3de0f82a4b49f6452e17e
ivangsp <talk2gspivan@outlook.com>
2018-02-08 00:50:53 +0200

    added receipt component

--- src/components/Receipts.vue
+++ src/components/Receipts.vue
@@ -1,42 +1,136 @@
 <template>
-  <v-expansion-panel expand>
-    <v-expansion-panel-content v-for="item in receipts" :key="item.id" v-bind:value="item.id === 1">
-      <div slot="header">{{item.date}}</div>
-      <v-card>
-        <v-card-text class="lighten-2">
-         <v-layout row wrap>
-           <v-flex md2>
-             <div class="receipt grey text-md-center">
-               <h1><sup>$</sup> 25 </h1>
-               <h5 class="">Remi supermarket</h5>
-             </div>
-           </v-flex> <v-flex md2>
-             <div class="receipt grey text-md-center">
-               <h1><sup>$</sup> 25 </h1>
-               <h5 class="">Remi supermarket</h5>
-             </div>
-           </v-flex> <v-flex md2>
-             <div class="receipt grey text-md-center ">
-               <h1><sup>$</sup> 25 </h1>
-               <h5 class="">Remi supermarket</h5>
-             </div>
-           </v-flex> <v-flex md2>
-             <div class="receipt grey text-md-center">
-               <h1><sup>$</sup> 25 </h1>
-               <h5 class="">Remi supermarket</h5>
-             </div>
-           </v-flex>
-         </v-layout>
-        </v-card-text>
-      </v-card>
-    </v-expansion-panel-content>
-  </v-expansion-panel>
+  <div>
+    <v-expansion-panel expand>
+      <v-expansion-panel-content v-for="item in receipts" :key="item.id" v-bind:value="item.id === 1">
+        <div slot="header">{{item.date}}</div>
+        <v-card>
+          <v-card-text class="lighten-2">
+            <v-layout row wrap class="receipt ">
+
+              <v-flex md2 >
+                <v-btn @click.native.stop="dialog = true" class=" grey">
+                  <div class="" >
+                    <h1><sup>$</sup> 25 </h1>
+                    <h5 class="">Remi supermarket</h5>
+                  </div>
+                </v-btn>
+              </v-flex>
+
+              <v-flex md2>
+                <v-btn @click.native.stop="dialog = true" class=" grey">
+                  <div class="" >
+                    <h1><sup>$</sup> 25 </h1>
+                    <h5 class="">Remi supermarket</h5>
+                  </div>
+                </v-btn>
+              </v-flex>
+
+              <v-flex md2>
+                <v-btn @click.native.stop="dialog = true" class=" grey">
+                  <div class="" >
+                    <h1><sup>$</sup> 25 </h1>
+                    <h5 class="">Remi supermarket</h5>
+                  </div>
+                </v-btn>
+              </v-flex>
+
+              <v-flex md2>
+                <v-btn @click.native.stop="dialog = true" class=" grey">
+                  <div class="" >
+                    <h1><sup>$</sup> 25 </h1>
+                    <h5 class="">Remi supermarket</h5>
+                  </div>
+                </v-btn>
+              </v-flex>
+
+            </v-layout>
+          </v-card-text>
+        </v-card>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+
+
+    <v-layout row >
+      <v-dialog v-model="dialog" max-width="450">
+        <v-card>
+          <v-toolbar color="cyan" dark>
+            <v-toolbar-title>Rimi super market</v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+          <table class="table table-striped">
+            <tbody>
+            <tr>
+              <td scope="col-md-9">Mark</td>
+              <td scope="col-md-3">Otto</td>
+            </tr>
+            <tr>
+              <td scope="col-md-9">Mark</td>
+              <td scope="col-md-3">Otto</td>
+            </tr>
+            <tr>
+              <td scope="col-md-9">Mark</td>
+              <td scope="col-md-3">Otto</td>
+            </tr>
+
+            </tbody>
+          </table>
+
+          <div class="row">
+            <div class="col-auto">
+              <div class="input-group mb-2">
+                <div class="input-group-prepend">
+                  <div class="input-group-text">Add tag</div>
+                </div>
+                <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="eg fruits">
+                  <button class="btn btn-primary md-2">Add</button>
+              </div>
+            </div>
+
+          </div>
+          <v-divider></v-divider>
+
+          <div class="row">
+
+            <div class="col-auto">
+              <div class="form-check mb-2">
+                <input class="form-check-input" type="checkbox" id="autoSizingCheck1">
+                <label class="form-check-label" for="autoSizingCheck1">
+                  Fruits
+                </label>
+              </div>
+            </div><div class="col-auto">
+              <div class="form-check mb-2">
+                <input class="form-check-input" type="checkbox" id="autoSizingCheck2">
+                <label class="form-check-label" for="autoSizingCheck2">
+                  cereals
+                </label>
+              </div>
+            </div>
+            <div class="col-auto">
+              <div class="form-check mb-2">
+                <input class="form-check-input" type="checkbox" id="autoSizingCheck">
+                <label class="form-check-label" for="autoSizingCheck">
+                  Alchol
+                </label>
+              </div>
+            </div>
+          </div>
+          <v-card-actions>
+            <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+    </v-layout>
+  </div>
+
 </template>
 
 <script>
   export default {
     data(){
       return{
+        dialog: false,
         receipts:[
           { id:   1,
             date: '08-feb-2018',
@@ -79,9 +173,16 @@
   }
 </script>
 <style lang="scss" scoped>
-  .receipt{
-    margin-right: 5px;
+  .receipt
+  .btn{
+    min-height: 100px;
+  }
 
+  .receipt
+  .flex.md2 {
+    -ms-flex-preferred-size: 16.666666666666664%;
+    flex-basis: 16.666666666666664%;
+    max-width: 12.666666666666664%;
   }
 
 </style>
