5279f856ea5fe760a2b7069a098a4e4184405389
Maksym Melnyk <imaxmelnyk@gmail.com>
2018-02-07 23:29:52 +0200

    stat and design

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,145 @@
 <template>
   <v-app>
     <v-navigation-drawer
+      class="white"
+      width="400"
+      floating
+      clipped
       fixed
-      v-model="drawer"
       app
     >
-      <v-list dense>
-        <v-list-tile to="/receipts" exact>
-          <v-list-tile-action>
-            <v-icon>receipt</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Receipts</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile to="statistics" exact>
-          <v-list-tile-action>
-            <v-icon>multiline_chart</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Statistics</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile @click="logout">
-          <v-list-tile-action>
-            <v-icon>power_settings_new</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Log out</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-divider></v-divider>
-        <v-subheader>Labels</v-subheader>
-        <v-list-tile v-for="label in labels" :key="label.title" @click="">
-          <v-list-tile-action>
-            <v-icon>label</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ label.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+      <v-layout row wrap>
+        <v-flex md12>
+          <v-text-field
+            prepend-icon="search"
+            label="Search"
+          ></v-text-field>
+        </v-flex>
+        <v-flex md6>
+          <div class="inline-heading">Filter search by:</div>
+        </v-flex>
+        <v-flex  md6>
+          <v-radio-group row>
+            <v-radio label="Price"></v-radio>
+            <v-radio label="Place"></v-radio>
+          </v-radio-group>
+        </v-flex>
+        <v-flex md12>
+          <div class="inline-heading">Search by date:</div>
+        </v-flex>
+        <v-flex md12>
+          <vc-date-picker
+            mode='range'
+            v-model='selectedDate'
+            is-inline
+            show-caps
+            is-expanded
+            :theme-styles='calendarStyles'
+            drag-color="#22b4fc"
+            select-color="#0092da"
+            :month-labels='["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]'
+            :weekday-labels='["MO", "TU", "WE", "TH", "FR", "SA", "SU"]'
+          ></vc-date-picker>
+        </v-flex>
+        <v-flex md12>
+          <v-divider></v-divider>
+          <v-list>
+            <v-list-group>
+              <v-list-tile slot="item">
+                <v-list-tile-action>
+                  <v-icon>flag</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title>Tags</v-list-tile-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                  <v-icon>keyboard_arrow_down</v-icon>
+                </v-list-tile-action>
+              </v-list-tile>
+              <v-list-tile v-for="tag in tags" :key="tag.title" @click="">
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    <v-icon>chevron_right</v-icon>
+                    {{ tag.title }}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list-group>
+          </v-list>
+        </v-flex>
+      </v-layout>
     </v-navigation-drawer>
-    <v-toolbar color="teal" dark fixed app>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <img id="logo" src="./assets/logo.png" @click="$router.push('/receipts')">
+    <v-toolbar
+      class="black"
+      height="100px"
+      clipped-left
+      flat
+      fixed
+      app
+    >
+      <img id="logo" src="./assets/logo.svg" @click="$router.push('/receipts')">
+      <v-toolbar-items id="menu">
+        <v-btn class="menu-item" active-class="active-menu-item" depressed to="/receipts" exact>Receipts</v-btn>
+        <v-btn class="menu-item" active-class="active-menu-item" depressed to="/statistics" exact>Statistics</v-btn>
+      </v-toolbar-items>
+      <div class="align-center" style="margin-left: auto">
+        <div class="hello-user">Hello, Ucha!</div>
+        <v-menu offset-y>
+          <v-avatar size="50px" slot="activator">
+            <img src="./assets/icon.svg">
+          </v-avatar>
+          <v-list>
+            <v-list-tile @click="">
+              <v-list-tile-action>
+                <v-icon>account_circle</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-title>Profile</v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile @click="logout">
+              <v-list-tile-action>
+                <v-icon>power_settings_new</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-title>Logout</v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
+      </div>
+
     </v-toolbar>
-    <v-content>
-      <v-container fluid fill-height>
+    <v-content class="white">
+      <v-container fluid>
         <v-slide-x-transition mode="out-in">
           <router-view></router-view>
         </v-slide-x-transition>
       </v-container>
     </v-content>
-    <v-footer color="teal" app>
-      <span class="white--text">&copy; 2018 eReceipt</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
   export default {
     data: () => ({
-      drawer: false,
-      labels: [{
-        title: 'Products'
+      tags: [{
+        title: 'Gas'
       }, {
-        title: 'Electronics'
-      }]
+        title: 'Food'
+      }, {
+        title: 'Clothing'
+      }],
+      selectedDate: {
+        start: new Date(2018, 0, 9),
+        end: new Date(2018, 0, 18)
+      },
+      calendarStyles: {
+        wrapper: {
+          border: 0,
+          background: 'white'
+        },
+        weekdaysHorizontalDivider: {
+          borderTop: 'solid #ccc 1px',
+          width: '100%'
+        },
+      }
     }),
     methods: {
       logout: () => {
@@ -79,7 +151,42 @@
 
 <style lang="scss" scoped>
   img#logo {
-    height: 150%;
-    cursor: pointer;
+    height: 60px;
+  }
+  .hello-user {
+    display: inline-block;
+    font-size: 12pt;
+    font-weight: bold;
+    letter-spacing: 1px;
+    color: #ababab;
+  }
+
+  #menu {
+    padding-top: 60px;
+    margin-left: auto;
+
+    .menu-item {
+      width: 300px;
+      border-radius: 0px;
+      background-color: #c3c3c3;
+    }
+  }
+
+  .active-menu-item {
+    background-color: #0092da !important;
+  }
+
+  .navigation-drawer {
+    padding: 50px;
+  }
+
+  .radio-group {
+    padding: 0px;
+  }
+
+  .inline-heading {
+    line-height: 30px;
+    font-size: 12pt;
+    color: #666;
   }
 </style>
--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -1,13 +1,50 @@
 <template>
-  <div>
-    Graphs
-  </div>
+  <v-layout row wrap>
+    <v-flex md4 sm12>
+      <line-chart :chart-data="monthlyChart"></line-chart>
+    </v-flex>
+    <v-flex md4 sm12>
+      <pie-chart :chart-data="shopsChart"></pie-chart>
+    </v-flex>
+    <v-flex md4 sm12>
+      <pie-chart :chart-data="monthlyChart"></pie-chart>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-export default {
-  data: () => ({
+  import LineChart from './LineChart.js'
+  import PieChart from './PieChart.js'
 
-  })
-}
+  export default {
+    components: { LineChart, PieChart },
+    data: () => ({
+      monthlyChart: {
+        labels: [...Array(4).keys()],
+        datasets: [
+          {
+            label: 'Monthly spending',
+            backgroundColor: ['#f87929','#f81979','#f17979','#f87974'],
+            data: [40, 20, 10, 40]
+          }
+        ]
+      },
+      shopsChart: {
+        labels: ['Rimi', 'Maksi', 'Comarket', 'Konsum'],
+        datasets: [
+          {
+            label: 'Shops',
+            backgroundColor: ['#f87929','#f81979','#f17979','#f87974'],
+            data: [40, 20, 10, 40]
+          }
+        ]
+      }
+    }),
+    mounted () {
+      this.fillData()
+    },
+    methods: {
+      fillData () {}
+    }
+  }
 </script>
